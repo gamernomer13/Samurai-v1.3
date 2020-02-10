a416fdd68486e64e57bd4b268140e92f234e5d3b
unknown <gamer-nomer13@mail.ru>
2020-02-10 15:43:39 +0300

    laba 4.1

--- 1/Program.cs
+++ 1/Program.cs
@@ -8,7 +8,31 @@ namespace laba3_4
     {
         static void Main(string[] args)
         {
-           
+            try
+            {
+                double a = 0, b = 0, c = 0;
+                Console.WriteLine("ente 3 parameters a, b and c");
+                a = Convert.ToDouble(Console.ReadLine());
+                b = Convert.ToDouble(Console.ReadLine());
+                c = Convert.ToDouble(Console.ReadLine());
+                MyLog.log("The equation is introduced: " + a + "X^2+" + b + "X+" + c + "=0");
+                QuadraticEquation qe = new QuadraticEquation();
+                List<double> list = qe.solve(a, b, c);
+                String s = "Roots: ";
+                foreach (double i in list)
+                {
+                    s += i + " ";
+                }
+
+                MyLog.log(s);
+
+            }
+            catch (KuzecovExeption e)
+            {
+                MyLog.log("Error: " + e.Message);
+            }
+            MyLog.write();
+            Console.ReadKey();
         }
     }
 }
