daf3197bcea0e7dd6b6d2c11529cd409ecbb1dae
unknown <gamer-nomer13@mail.ru>
2020-02-10 14:51:57 +0300

    laba 1

---
+++ 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1
+{
+   
+     class A
+    {
+
+    }
+     class C:A
+    {
+       protected object first;
+        public C(object first)
+        {
+            this.first = first;
+        }
+
+    }
+
+     class B : C
+    {
+       protected object second;
+        public B(object first,object second):base(first)
+        {
+            this.first = first;
+            this.second = second;
+        }
+    }   
+    class program
+    {
+        object a = new A();
+        B b = new B(new C(a), new B(a, new C(a)));
+    }
+     
+}
